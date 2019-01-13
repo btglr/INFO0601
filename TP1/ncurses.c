b1a210de5699684dac4648d0eeffd02636bbf390
BstTag <bastien.tagliaro@gmail.com>
2019-01-13 14:14:07 +0100

    Changed formatting

--- TP1/ncurses.c
+++ TP1/ncurses.c
@@ -1,61 +1,61 @@
 #include "ncurses.h"
 
-#include <curses.h>   /* Pour toutes les fonctions/constantes ncurses */
-#include <stdlib.h>    /* Pour exit, EXIT_FAILURE */
+#include <curses.h> /* Pour toutes les fonctions/constantes ncurses */
+#include <stdlib.h> /* Pour exit, EXIT_FAILURE */
 
 /**
  * Initialisation de ncurses.
  */
 void ncurses_initialiser() {
-  initscr();	        /* Demarre le mode ncurses */
-  cbreak();	          /* Pour les saisies clavier (desac. mise en buffer) */
-  noecho();             /* Desactive l'affichage des caracteres saisis */
-  keypad(stdscr, TRUE);	/* Active les touches specifiques */
-  refresh();            /* Met a jour l'affichage */
-  curs_set(FALSE);      /* Masque le curseur */
+    initscr();            /* Demarre le mode ncurses */
+    cbreak();             /* Pour les saisies clavier (desac. mise en buffer) */
+    noecho();             /* Desactive l'affichage des caracteres saisis */
+    keypad(stdscr, TRUE); /* Active les touches specifiques */
+    refresh();            /* Met a jour l'affichage */
+    curs_set(FALSE);      /* Masque le curseur */
 }
 
 /**
  * Fin de ncurses.
  */
 void ncurses_stopper() {
-  endwin();
+    endwin();
 }
 
 /**
  * Initialisation des couleurs.
  */
 void ncurses_couleurs() {
-  /* Verification du support de la couleur */
-  if(has_colors() == FALSE) {
-    ncurses_stopper();
-    fprintf(stderr, "Le terminal ne supporte pas les couleurs.\n");
-    exit(EXIT_FAILURE);
-  }
+    /* Verification du support de la couleur */
+    if (has_colors() == FALSE) {
+        ncurses_stopper();
+        fprintf(stderr, "Le terminal ne supporte pas les couleurs.\n");
+        exit(EXIT_FAILURE);
+    }
 
-  /* Activation des couleurs */
-  start_color();
+    /* Activation des couleurs */
+    start_color();
 
-  /* Definition de la palette */
-  init_pair(1, COLOR_BLUE, COLOR_BLACK);
-  init_pair(2, COLOR_BLACK, COLOR_RED);
+    /* Definition de la palette */
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_RED);
 }
 
 /**
  * Initialisation de la souris.
  */
 void ncurses_souris() {
-  if(!mousemask(ALL_MOUSE_EVENTS, NULL)) {
-    ncurses_stopper();
-    fprintf(stderr, "Erreur lors de l'initialisation de la souris.\n");
-    exit(EXIT_FAILURE);
-  }
- 
-  if(has_mouse() != TRUE) {
-    ncurses_stopper();
-    fprintf(stderr, "Aucune souris n'est détectée.\n");
-    exit(EXIT_FAILURE);
-  }
+    if (!mousemask(ALL_MOUSE_EVENTS, NULL)) {
+        ncurses_stopper();
+        fprintf(stderr, "Erreur lors de l'initialisation de la souris.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (has_mouse() != TRUE) {
+        ncurses_stopper();
+        fprintf(stderr, "Aucune souris n'est détectée.\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 /**
@@ -66,13 +66,14 @@ void ncurses_souris() {
  * @return OK si reussite
  */
 int souris_getpos(int *x, int *y, int *bouton) {
-  MEVENT event;
-  int resultat = getmouse(&event);
- 
-  if(resultat == OK) {
-    *x = event.x;
-    *y = event.y;
-    if(bouton != NULL) *bouton = event.bstate;
-  }
-  return resultat;
+    MEVENT event;
+    int resultat = getmouse(&event);
+
+    if (resultat == OK) {
+        *x = event.x;
+        *y = event.y;
+        if (bouton != NULL)
+            *bouton = event.bstate;
+    }
+    return resultat;
 }
