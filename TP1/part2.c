3471a87ae181f52ece54585bd19e3e33a95bb724
BstTag <bastien.tagliaro@gmail.com>
2019-01-15 22:18:12 +0100

    Initialized part2 file

---
+++ TP1/part2.c
@@ -0,0 +1,39 @@
+#include <curses.h> /* Pour printw, attron, attroff, COLOR_PAIR, getch */
+#include <stdlib.h>
+#include <stdlib.h> /* Pour EXIT_FAILURE */
+#include <string.h>
+
+#include "ncurses.h"
+
+#define LARGEUR 20 /* Largeur de la fenêtre */
+#define HAUTEUR 10 /* Hauteur de la fenêtre */
+#define POSX 20    /* Position horizontale de la fenêtre */
+#define POSY 5     /* Position verticale de la fenêtre */
+
+int main() {
+    int i, color;
+    char* texte = "Bonjour";
+    WINDOW* fenetre;
+
+    /* Initialisation de ncurses */
+    ncurses_initialiser();
+    ncurses_souris();
+    ncurses_couleurs();
+
+    init_pair(3, COLOR_CYAN, COLOR_BLACK);
+    init_pair(4, COLOR_RED, COLOR_BLACK);
+    init_pair(5, COLOR_GREEN, COLOR_BLACK);
+
+    clear();
+    refresh();
+    /* Création de la fenêtre */
+    fenetre = newwin(HAUTEUR, LARGEUR, POSY, POSX);
+
+    /* Suppression de la fenêtre */
+    delwin(fenetre);
+
+    /* Arrêt de ncurses */
+    ncurses_stopper();
+
+    return EXIT_SUCCESS;
+}
