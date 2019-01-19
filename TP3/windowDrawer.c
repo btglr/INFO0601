d5e74b8476f61429ce1eb2729c158353907cd5fc
BstTag <bastien.tagliaro@gmail.com>
2019-01-19 17:15:08 +0100

    Added a method to draw a square on a window

--- TP3/windowDrawer.c
+++ TP3/windowDrawer.c
@@ -21,8 +21,32 @@ WINDOW *initializeSubWindow(WINDOW *window, int width, int height, int x, int y)
     return subwin(window, height, width, y, x);
 }
 
-void drawMap(WINDOW *window, int fd) {
+void drawSquare(WINDOW *window, int type, int x, int y, bool refresh) {
     chtype color;
+
+    switch(type) {
+        case VISIBLE_WALL:
+            color = COLOR_PAIR(5);
+            break;
+
+        case DISCOVERED_WALL:
+            color = COLOR_PAIR(4);
+            break;
+
+        default:
+            color = COLOR_PAIR(1);
+    }
+
+    wattron(window, color);
+    mvwprintw(window, y, x, "  ");
+    wattroff(window, color);
+
+    if(refresh) {
+        wrefresh(window);
+    }
+}
+
+void drawMap(WINDOW *window, int fd) {
     int i, x, y, initialPadding = sizeof(int) + sizeof(unsigned char);
     unsigned char buffer[MAP_WIDTH * MAP_HEIGHT];
     ssize_t bytesRead;
@@ -33,7 +57,9 @@ void drawMap(WINDOW *window, int fd) {
         x = (i % MAP_WIDTH) * SQUARE_WIDTH;
         y = i / MAP_WIDTH;
 
-        switch (buffer[i]) {
+        drawSquare(window, buffer[i], x, y, false);
+
+        /*switch (buffer[i]) {
             case VISIBLE_WALL:
                 color = COLOR_PAIR(5);
                 break;
@@ -48,8 +74,8 @@ void drawMap(WINDOW *window, int fd) {
 
         wattron(window, color);
         mvwprintw(window, y, x, "  ");
-        wattroff(window, color);
-
-        wrefresh(window);
+        wattroff(window, color);*/
     }
+
+    wrefresh(window);
 }
