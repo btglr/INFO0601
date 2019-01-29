ca368723dd08de68886608cc1c3c825992240cf5
BstTag <bastien.tagliaro@gmail.com>
2019-01-29 22:42:29 +0100

    Moved documentation from source to header. Translated to english.

--- TP3/ncurses.c
+++ TP3/ncurses.c
@@ -3,77 +3,58 @@
 #include <curses.h> /* Pour toutes les fonctions/constantes ncurses */
 #include <stdlib.h> /* Pour exit, EXIT_FAILURE */
 
-/**
- * Initialisation de ncurses.
- */
 void initialize_ncurses() {
-    initscr();            /* Demarre le mode ncurses */
-    cbreak();             /* Pour les saisies clavier (desac. mise en buffer) */
-    noecho();             /* Desactive l'affichage des caracteres saisis */
-    keypad(stdscr, TRUE); /* Active les touches specifiques */
-    refresh();            /* Met a jour l'affichage */
-    curs_set(FALSE);      /* Masque le curseur */
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    refresh();
+    curs_set(FALSE);
 }
 
-/**
- * Fin de ncurses.
- */
 void stop_ncurses() {
     endwin();
 }
 
-/**
- * Initialisation des couleurs.
- */
 void ncurses_colors() {
-    /* Verification du support de la couleur */
+    /* Checking if color is supported */
     if (has_colors() == FALSE) {
         stop_ncurses();
-        fprintf(stderr, "Le terminal ne supporte pas les couleurs.\n");
+        fprintf(stderr, "The terminal can't handle colors.\n");
         exit(EXIT_FAILURE);
     }
 
-    /* Activation des couleurs */
+    /* Activating the colors */
     start_color();
 
-    /* Definition de la palette */
+    /* Creating the color pairs */
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
     init_pair(2, COLOR_BLACK, COLOR_RED);
 }
 
-/**
- * Initialisation de la souris.
- */
 void ncurses_mouse() {
     if (!mousemask(ALL_MOUSE_EVENTS, NULL)) {
         stop_ncurses();
-        fprintf(stderr, "Erreur lors de l'initialisation de la souris.\n");
+        fprintf(stderr, "An error occurred while initializing the mouse.\n");
         exit(EXIT_FAILURE);
     }
 
     if (has_mouse() != TRUE) {
         stop_ncurses();
-        fprintf(stderr, "Aucune souris n'est détectée.\n");
+        fprintf(stderr, "Error: No mouse detected.\n");
         exit(EXIT_FAILURE);
     }
 }
 
-/**
- * Recupere la position x et y de la souris.
- * @param[out] x la position en x
- * @param[out] y la position en y
- * @param[out] button l'évenement associé au clic (ou NULL)
- * @return OK si reussite
- */
 int mouse_getpos(int *x, int *y, int *button) {
     MEVENT event;
-    int resultat = getmouse(&event);
+    int result = getmouse(&event);
 
-    if (resultat == OK) {
+    if (result == OK) {
         *x = event.x;
         *y = event.y;
         if (button != NULL)
             *button = event.bstate;
     }
-    return resultat;
+    return result;
 }
--- TP3/ncurses.h
+++ TP3/ncurses.h
@@ -1,32 +1,38 @@
+/**
+ * @file ncurses.h
+ * @author Cyril R.
+ * @brief Utility functions for ncurses
+ */
+
 #ifndef _NCURSES_
 #define _NCURSES_
 
 /**
- * Initialisation de ncurses.
+ * Initializes ncurses
  */
 void initialize_ncurses();
 
 /**
- * Fin de ncurses.
+ * Stops ncurses
  */
 void stop_ncurses();
 
 /**
- * Initialisation des couleurs.
+ * Initializes ncurses colors
  */
 void ncurses_colors();
 
 /**
- * Initialisation de la souris.
+ * Initializes the mouse
  */
 void ncurses_mouse();
 
 /**
- * Recupere la position x et y de la souris.
- * @param[out] x la position en x
- * @param[out] y la position en y
- * @param[out] button l'évenement associé au clic (ou NULL)
- * @return OK si réussite
+ * Gets the mouse's X and Y position
+ * @param[out] x the x position
+ * @param[out] y the y position
+ * @param[out] button the event associated to the click (or NULL)
+ * @return
  */
 int mouse_getpos(int *x, int *y, int *button);
 
