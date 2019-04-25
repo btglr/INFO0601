b4405ab5c248978910effc6190e8dc1e95e232c3
BstTag <bastien.tagliaro@gmail.com>
2019-04-25 17:31:41 +0200

    Implementation based on union

--- Projet3/server.c
+++ Projet3/server.c
@@ -1,9 +1,19 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <signal.h>
+#include <curses.h>
 #include "queue.h"
 #include "socketUtils.h"
 #include "structures.h"
+
+bool run = TRUE;
+
+void handler(int sig) {
+    if (sig == SIGINT) {
+        run = FALSE;
+    }
+}
 
 int main(int argc, char *argv[]) {
     int sock, port;
@@ -12,6 +22,12 @@ int main(int argc, char *argv[]) {
     struct sockaddr clientAddress;
     queue_t *queue;
     request_connect_t request_connect;
+    connect_udp_master_t masterConnect;
+    connect_udp_slave_t slaveConnect;
+    queue_element_t *qElem, *masterClient, *slaveClient;
+    send_address_udp_t response_connect;
+    struct sigaction action;
+    ssize_t bytesReceived;
 
     if (argc < 2) {
         fprintf(stderr, "At least 1 argument is required\n");
@@ -22,31 +38,66 @@ int main(int argc, char *argv[]) {
     sock = createSocket(SOCK_DGRAM, IPPROTO_UDP);
 
     initAddressAny(&serverAddress, port);
+    bindAddress(sock, &serverAddress);
 
-    memset(&clientAddress, 0, sizeof(struct sockaddr));
+    sigemptyset(&action.sa_mask);
+    action.sa_handler = handler;
 
-    if (bind(sock, (struct sockaddr*) &serverAddress, sizeof(struct sockaddr_in)) == -1) {
-        perror("An error occurred while binding the socket");
+    if (sigaction(SIGINT, &action, NULL) == -1) {
+        fprintf(stderr, "An error occurred while catching the SIGINT signal\n");
         exit(EXIT_FAILURE);
     }
 
-    queue = createQueue(10);
+    queue = createQueue(QUEUE_SIZE);
+    memset(&clientAddress, 0, sizeof(struct sockaddr));
 
-    while (1) {
-        if (recvfrom(sock, &request_connect, sizeof(request_connect_t), 0, &clientAddress, &clientAddressLength) == -1) {
-            perror("An error occurred while waiting to receive the message");
-            exit(EXIT_FAILURE);
+    while (run) {
+        clientAddressLength = sizeof(struct sockaddr_in);
+        bytesReceived = receiveUDP(sock, &request_connect, sizeof(request_connect_t), &clientAddress, &clientAddressLength);
+
+        if (bytesReceived != -1) {
+            if (request_connect.type == TYPE_CONNECT_UDP_SLAVE || request_connect.type == TYPE_CONNECT_UDP_MASTER) {
+                qElem = (queue_element_t *) malloc(sizeof(queue_element_t));
+                qElem->request = request_connect;
+                qElem->sourceAddr = clientAddress;
+                qElem->addrLen = clientAddressLength;
+
+                enqueue(queue, qElem);
+            }
+
+            /* If two clients are connected, dequeue */
+            if (queue->size == QUEUE_SIZE) {
+                if (front(queue)->request.type == TYPE_CONNECT_UDP_MASTER && rear(queue)->request.type == TYPE_CONNECT_UDP_SLAVE) {
+                    masterClient = dequeue(queue);
+                    slaveClient = dequeue(queue);
+                }
+
+                else if (front(queue)->request.type == TYPE_CONNECT_UDP_SLAVE && rear(queue)->request.type == TYPE_CONNECT_UDP_MASTER) {
+                    slaveClient = dequeue(queue);
+                    masterClient = dequeue(queue);
+                }
+
+                else {
+                    fprintf(stderr, "The server requires a Master client and a Slave client");
+                    exit(EXIT_FAILURE);
+                }
+
+                response_connect.port = masterClient->request.request.master.port;
+
+                if (inet_ntop(AF_INET, &((struct sockaddr_in*) &masterClient->sourceAddr)->sin_addr, response_connect.address, INET_ADDRSTRLEN) == NULL) {
+                    exit(EXIT_FAILURE);
+                }
+
+                printf("Sending address and TCP port to the Slave client\n");
+                sendUDP(sock, &response_connect, sizeof(send_address_udp_t), &slaveClient->sourceAddr, slaveClient->addrLen);
+
+                free(slaveClient);
+                free(masterClient);
+            }
         }
-
-        if (request_connect.type == TYPE_CONNECT_UDP_MASTER) {
-
-        }
-
-        else if (request_connect.type == TYPE_CONNECT_UDP_SLAVE) {
-
-        }
-
-        printf("Received request\n");
-        enqueue(queue, "Lol");
     }
+
+    printf("\tStopped server\n");
+
+    return EXIT_SUCCESS;
 }
