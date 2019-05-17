46d5319192b0b45963c43a8bef2d9d16eddf18bd
BstTag <bastien.tagliaro@gmail.com>
2019-05-17 15:26:09 +0200

    Added an update message queue

---
+++ Projet3/structures/updateQueue.c
@@ -0,0 +1,80 @@
+#include <string.h>
+#include "../utils/memoryUtils.h"
+#include "updateQueue.h"
+
+updateQueue_t *createUpdateQueue(unsigned capacity) {
+    updateQueue_t *queue = (updateQueue_t *) malloc_check(sizeof(updateQueue_t));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+    queue->rear = capacity - 1;
+    queue->messages = (updateMessage_t**) malloc_check(sizeof(updateMessage_t*) * queue->capacity);
+
+    return queue;
+}
+
+void destroyUpdateQueue(updateQueue_t *queue) {
+    updateMessage_t *elem;
+
+    while (!isUpdateQueueEmpty(queue)) {
+        elem = dequeueUpdate(queue);
+        free(elem);
+    }
+
+    free(queue);
+}
+
+int isUpdateQueueFull(updateQueue_t *queue) {
+    return (queue->size == queue->capacity);
+}
+
+int isUpdateQueueEmpty(updateQueue_t *queue) {
+    return (queue->size == 0);
+}
+
+void enqueueMessage(updateQueue_t *queue, WINDOW *window, int chunkId) {
+    if (isUpdateQueueFull(queue))
+        return;
+
+    updateMessage_t *item = (updateMessage_t*) malloc(sizeof(updateMessage_t));
+    item->window = window;
+    item->chunkId = chunkId;
+
+    enqueueUpdate(queue, item);
+}
+
+void enqueueUpdate(updateQueue_t *queue, updateMessage_t *item) {
+    if (isUpdateQueueFull(queue))
+        return;
+
+    queue->rear = (queue->rear + 1) % queue->capacity;
+
+    queue->messages[queue->rear] = item;
+    queue->size = queue->size + 1;
+}
+
+updateMessage_t *dequeueUpdate(updateQueue_t *queue) {
+    updateMessage_t *item;
+
+    if (isUpdateQueueEmpty(queue))
+        return NULL;
+
+    item = queue->messages[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size = queue->size - 1;
+
+    return item;
+}
+
+updateMessage_t *frontUpdateQueue(updateQueue_t *queue) {
+    if (isUpdateQueueEmpty(queue))
+        return NULL;
+
+    return queue->messages[queue->front];
+}
+
+updateMessage_t *rearUpdateQueue(updateQueue_t *queue) {
+    if (isUpdateQueueEmpty(queue))
+        return NULL;
+
+    return queue->messages[queue->rear];
+}
---
+++ Projet3/structures/updateQueue.h
@@ -0,0 +1,31 @@
+#ifndef TP9_QUEUE_H
+#define TP9_QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <curses.h>
+#include "structures.h"
+
+typedef struct {
+    WINDOW *window;
+    int chunkId;
+} updateMessage_t;
+
+typedef struct updateQueue {
+    int front, rear, size;
+    unsigned capacity;
+    updateMessage_t **messages;
+    pthread_mutex_t *mutex;
+} updateQueue_t;
+
+updateQueue_t* createUpdateQueue(unsigned capacity);
+int isUpdateQueueFull(updateQueue_t* queue);
+int isUpdateQueueEmpty(updateQueue_t* queue);
+void enqueueUpdate(updateQueue_t *queue, updateMessage_t *item);
+updateMessage_t *dequeueUpdate(updateQueue_t *queue);
+updateMessage_t *frontUpdateQueue(updateQueue_t *queue);
+updateMessage_t *rearUpdateQueue(updateQueue_t *queue);
+void destroyUpdateQueue(updateQueue_t* queue);
+
+#endif
