f302cd074e8aa54f89a7b1463d8eaeb649d63be6
BstTag <bastien.tagliaro@gmail.com>
2019-05-17 23:40:50 +0200

    Functions:
    - Place lemming
    - Freeze lemming
    - Explode lemming
    - Remove lemming

--- Projet3/utils/gameUtils.c
+++ Projet3/utils/gameUtils.c
@@ -1,11 +1,18 @@
+#include <string.h>
 #include <curses.h>
 #include <pthread.h>
 #include "gameUtils.h"
 #include "windowDrawer.h"
 #include <stdlib.h>
+#include <thread_db.h>
 #include "../lemming.h"
 #include "memoryUtils.h"
 #include "chunkManager.h"
+#include "threadUtils.h"
+
+extern pthread_mutex_t displayMutex;
+
+pthread_mutex_t displayMutex;
 
 void initializeGame(int mapWidth, int mapHeight, char *map, WINDOW **borderInformationWindow,
                     WINDOW **informationWindow, WINDOW **borderGameWindow, WINDOW **gameWindow,
@@ -18,25 +25,6 @@ void initializeGame(int mapWidth, int mapHeight, char *map, WINDOW **borderInfor
     *gameWindow = createGameWindow(*borderGameWindow, mapWidth, mapHeight);
 
     drawMap(*gameWindow, mapWidth, mapHeight, map);
-}
-
-void placeLemming(WINDOW *window, map_t *map, int posX, int posY) {
-    int chunkId, chunkPos;
-    coord_t *coords;
-
-    coords = (coord_t*) malloc_check(sizeof(coord_t));
-    coords->x = posX / SQUARE_WIDTH;
-    coords->y = posY;
-
-    chunkId = getChunk(coords, map);
-
-    chunkPos = globalToLocalCoordinate(coords, map->chunkSize);
-
-    map->chunks[chunkId].squares[chunkPos].type = LEMMING;
-    updateChunk(window, map, chunkId);
-
-    map->chunks[chunkId].squares[chunkPos].lemming = (pthread_t*) malloc_check(sizeof(pthread_t));
-    pthread_create(map->chunks[chunkId].squares[chunkPos].lemming, NULL, lemming_thread, coords);
 }
 
 int getTool(int mouseX, int mouseY) {
@@ -74,4 +62,95 @@ int getTool(int mouseX, int mouseY) {
 
 int makeMultipleOf(int v, int m) {
     return v - (v % m);
+}
+
+bool placeLemming(WINDOW *window, lemming_t *lemming, map_t *map, int posX, int posY) {
+    int chunkId, chunkPos;
+    lemming_data_t *lemmingData;
+    bool placed = false;
+
+    lemmingData = (lemming_data_t*) malloc(sizeof(lemming_data_t));
+
+    lemmingData->coords = (coord_t*) malloc_check(sizeof(coord_t));
+    lemmingData->coords->x = posX / SQUARE_WIDTH;
+    lemmingData->coords->y = posY;
+
+    lemmingData->pipe = lemming->pipe;
+
+    chunkId = getChunk(lemmingData->coords, map);
+
+    chunkPos = globalToLocalCoordinate(lemmingData->coords, map->chunkSize);
+
+    if (!lemming->dead && lemming->thread == NULL) {
+        map->chunks[chunkId].squares[chunkPos].type = LEMMING;
+
+        updateChunk(window, map, chunkId);
+
+        lemming->thread = (pthread_t*) malloc_check(sizeof(pthread_t));
+        create_thread_check(lemming->thread, lemming_thread, lemmingData);
+
+        map->chunks[chunkId].squares[chunkPos].lemming = lemming;
+        placed = true;
+    }
+
+    return placed;
+}
+
+void removeLemming(WINDOW *window, lemming_t *lemming, map_t *map, int chunkId, int chunkPos) {
+    pthread_cancel(*lemming->thread);
+
+    map->chunks[chunkId].squares[chunkPos].type = EMPTY;
+    map->chunks[chunkId].squares[chunkPos].lemming->timesRemoved += 1;
+
+    updateChunk(window, map, chunkId);
+
+    /*char buf[3];
+
+    write(lemming->pipe[WRITE], "STOP", 5);
+    read(lemming->pipe[READ], buf, 3);
+
+    if (strcmp(buf, "OK") == 0) {
+        lemming->thread = NULL;
+    }*/
+}
+
+void initializeLemmings(lemming_t *lemmings, int nbLemmings) {
+    int i;
+
+    for (i = 0; i < nbLemmings; ++i) {
+        lemmings[i].timesRemoved = 0;
+        lemmings[i].id = i;
+        lemmings[i].thread = NULL;
+        lemmings[i].dead = FALSE;
+
+        pthread_mutex_init(&lemmings[i].action.mutex, NULL);
+        pthread_cond_init(&lemmings[i].action.cond, NULL);
+        lemmings[i].action.value = false;
+
+        /* TODO Initialize pipe */
+    }
+}
+
+void freezeLemming(lemming_t *lemming, map_t *map) {
+    mutex_lock_check(&lemming->action.mutex);
+
+    lemming->action.value = FREEZE;
+
+    pthread_cond_signal(&lemming->action.cond);
+
+    mutex_unlock_check(&lemming->action.mutex);
+}
+
+void explodeLemming(WINDOW *window, lemming_t *lemming, map_t *map, int chunkId, int chunkPos) {
+    mutex_lock_check(&lemming->action.mutex);
+
+    lemming->action.value = EXPLODE;
+
+    pthread_cond_signal(&lemming->action.cond);
+
+    mutex_unlock_check(&lemming->action.mutex);
+
+    map->chunks[chunkId].squares[chunkPos].type = EMPTY;
+
+    updateChunk(window, map, chunkId);
 }
--- Projet3/utils/gameUtils.h
+++ Projet3/utils/gameUtils.h
@@ -1,13 +1,19 @@
 #ifndef PROJET3_GAMEUTILS_H
 #define PROJET3_GAMEUTILS_H
 
+#include <thread_db.h>
 #include "../structures/structures.h"
 
 void initializeGame(int mapWidth, int mapHeight, char *map, WINDOW **borderInformationWindow,
                     WINDOW **informationWindow, WINDOW **borderGameWindow, WINDOW **gameWindow,
                     WINDOW **borderStateWindow, WINDOW **stateWindow);
-void placeLemming(WINDOW *window, map_t *map, int posX, int posY);
+bool placeLemming(WINDOW *window, lemming_t *lemming, map_t *map, int posX, int posY);
 int getTool(int mouseX, int mouseY);
 int makeMultipleOf(int v, int m);
+void removeLemming(WINDOW *window, lemming_t *lemming, map_t *map, int chunkId, int chunkPos);
+void freezeLemming(lemming_t *lemming, map_t *map);
+void explodeLemming(WINDOW *window, lemming_t *lemming, map_t *map, int chunkId, int chunkPos);
+lemming_t *getLemmingAtPos(lemming_t *lemmings, int pos);
+void initializeLemmings(lemming_t *lemmings, int nbLemmings);
 
 #endif
