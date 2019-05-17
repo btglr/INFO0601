0e7c2961fdcae6f02c52113071616db8400a1d43
BstTag <bastien.tagliaro@gmail.com>
2019-05-17 23:41:10 +0200

    Created utils functions for threads

--- Projet3/utils/threadUtils.c
+++ Projet3/utils/threadUtils.c
@@ -1,5 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "threadUtils.h"
+#include "ncurses.h"
 
-void test() {
+void mutex_lock_check(pthread_mutex_t *mutex) {
+    int returnValue;
 
+    if ((returnValue = pthread_mutex_lock(mutex)) != 0) {
+        stop_ncurses();
+        fprintf(stderr, "An error occurred while trying to lock the mutex: error %d\n", returnValue);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void create_thread_check(pthread_t *thread, void *(*func)(void *), void *arg) {
+    int returnValue;
+
+    if ((returnValue = pthread_create(thread, NULL, func, arg)) != 0) {
+        stop_ncurses();
+        fprintf(stderr, "An error occurred while creating the thread: error %d\n", returnValue);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void mutex_unlock_check(pthread_mutex_t *mutex) {
+    int returnValue;
+
+    if ((returnValue = pthread_mutex_unlock(mutex)) != 0) {
+        stop_ncurses();
+        fprintf(stderr, "An error occurred while trying to unlock the mutex, error: %d\n", returnValue);
+        exit(EXIT_FAILURE);
+    }
 }
--- Projet3/utils/threadUtils.h
+++ Projet3/utils/threadUtils.h
@@ -1,6 +1,10 @@
 #ifndef PROJET3_THREADUTILS_H
 #define PROJET3_THREADUTILS_H
 
-void test();
+#include <pthread.h>
+
+void mutex_lock_check(pthread_mutex_t *mutex);
+void mutex_unlock_check(pthread_mutex_t *mutex);
+void create_thread_check(pthread_t *thread, void *(*func) (void *), void *arg);
 
 #endif
